034f770567510c7043391175a343e0bf51ebe629
José Naves <josenaves@gmail.com>
2019-09-01 12:35:17 -0300

    refactor: Use Consumer

--- lib/models/task_provider.dart
+++ lib/models/task_provider.dart
@@ -1,19 +1,30 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:todoey_flutter/models/task.dart';
 
 class TaskProvider extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy milk'),
     Task(name: 'Buy egges'),
     Task(name: 'Buy breads'),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
   void addTask(String name) {
-    tasks.add(Task(name: name));
+    final task = Task(name: name);
+    _tasks.add(task);
     notifyListeners();
   }
 
-  void toggleValue(Task task) {
+  void updateTask(Task task) {
     task.toggleDone();
     notifyListeners();
   }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/models/task_provider.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final taskProvider = Provider.of<TaskProvider>(context);
-    final List<Task> tasks = taskProvider.tasks;
-
-    return ListView.builder(
+    return Consumer<TaskProvider>(builder: (context, taskProvider, child) {
+      return ListView.builder(
         itemBuilder: (context, index) {
+          final task = taskProvider.tasks[index];
           return TaskTile(
-            taskTitle: tasks[index].name,
-            isChecked: tasks[index].isDone,
-            checkboxCallback: (value) => taskProvider.toggleValue(tasks[index]),
+            taskTitle: task.name,
+            isChecked: task.isDone,
+            checkboxCallback: (value) => taskProvider.updateTask(task),
           );
         },
-        itemCount: tasks.length);
+        itemCount: taskProvider.taskCount,
+      );
+    });
   }
 }
